a8812fb1bbec5fcf169ea70bddab405f83e488a1
V.P.K <praveenv4k@gmail.com>
2015-02-21 16:31:16 +0900

    added additional projects to tracker

--- PCL_Tracker/OpenNISegmentTracking.h
+++ PCL_Tracker/OpenNISegmentTracking.h
@@ -206,6 +206,15 @@ public:
 		coherence->setSearchMethod(search);
 		coherence->setMaximumDistance(0.01);
 		tracker_->setCloudCoherence(coherence);
+
+		CloudPtr temp(new Cloud);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr tempCloud(new pcl::PointCloud<pcl::PointXYZ>);
+		if (pcl::io::loadPCDFile<pcl::PointXYZ>("NEXUS5.pcd", *tempCloud) == -1){
+			PCL_ERROR("Couldn't read the PCD file\n");
+		}
+		else{
+			//gridSample(temp, *ref_cloud_, downsampling_grid_size_);
+		}
 	}
 
 	bool
@@ -615,134 +624,15 @@ public:
 					trans.translation().matrix() = Eigen::Vector3f(c[0], c[1], c[2]);
 					//pcl::transformPointCloudWithNormals<RefPointType> (*ref_cloud, *transed_ref, trans.inverse());
 					pcl::transformPointCloud<RefPointType>(*nonzero_ref, *transed_ref, trans.inverse());
+					//Praveen
+#if 1
 					CloudPtr transed_ref_downsampled(new Cloud);
 					gridSample(transed_ref, *transed_ref_downsampled, downsampling_grid_size_);
 					tracker_->setReferenceCloud(transed_ref_downsampled);
-					tracker_->setTrans(trans);
-					reference_ = transed_ref;
-					tracker_->setMinIndices(int(ref_cloud->points.size()) / 2);
-				}
-				else
-				{
-					PCL_WARN("euclidean segmentation failed\n");
-				}
-			}
-		}
-		else
-		{
-			//normals_.reset (new pcl::PointCloud<pcl::Normal>);
-			//normalEstimation (cloud_pass_downsampled_, *normals_);
-			//RefCloudPtr tracking_cloud (new RefCloud ());
-			//addNormalToCloud (cloud_pass_downsampled_, normals_, *tracking_cloud);
-			//tracking_cloud = cloud_pass_downsampled_;
-
-			//*cloud_pass_downsampled_ = *cloud_pass_;
-			//cloud_pass_downsampled_ = cloud_pass_;
-			gridSampleApprox(cloud_pass_, *cloud_pass_downsampled_, downsampling_grid_size_);
-			tracking(cloud_pass_downsampled_);
-		}
-
-		new_cloud_ = true;
-		double end = pcl::getTime();
-		computation_time_ = end - start;
-		FPS_CALC_END("computation");
-		counter_++;
-	}
-
-	void
-		cloud_cb2(const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZRGBA>> &cloud)
-	{
-		boost::mutex::scoped_lock lock(mtx_);
-		double start = pcl::getTime();
-		FPS_CALC_BEGIN;
-		cloud_pass_.reset(new Cloud);
-		cloud_pass_downsampled_.reset(new Cloud);
-		pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
-		pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
-		filterPassThrough(cloud, *cloud_pass_);
-		if (counter_ < 10)
-		{
-			gridSample(cloud_pass_, *cloud_pass_downsampled_, downsampling_grid_size_);
-		}
-		else if (counter_ == 10)
-		{
-			//gridSample (cloud_pass_, *cloud_pass_downsampled_, 0.01);
-			cloud_pass_downsampled_ = cloud_pass_;
-			CloudPtr target_cloud;
-			if (use_convex_hull_)
-			{
-				planeSegmentation(cloud_pass_downsampled_, *coefficients, *inliers);
-				if (inliers->indices.size() > 3)
-				{
-					CloudPtr cloud_projected(new Cloud);
-					cloud_hull_.reset(new Cloud);
-					nonplane_cloud_.reset(new Cloud);
-
-					planeProjection(cloud_pass_downsampled_, *cloud_projected, coefficients);
-					convexHull(cloud_projected, *cloud_hull_, hull_vertices_);
-
-					extractNonPlanePoints(cloud_pass_downsampled_, cloud_hull_, *nonplane_cloud_);
-					target_cloud = nonplane_cloud_;
-				}
-				else
-				{
-					PCL_WARN("cannot segment plane\n");
-				}
-			}
-			else
-			{
-				PCL_WARN("without plane segmentation\n");
-				target_cloud = cloud_pass_downsampled_;
-			}
-
-			if (target_cloud != NULL)
-			{
-				PCL_INFO("segmentation, please wait...\n");
-				std::vector<pcl::PointIndices> cluster_indices;
-				euclideanSegment(target_cloud, cluster_indices);
-				if (cluster_indices.size() > 0)
-				{
-					// select the cluster to track
-					CloudPtr temp_cloud(new Cloud);
-					extractSegmentCluster(target_cloud, cluster_indices, 0, *temp_cloud);
-					Eigen::Vector4f c;
-					pcl::compute3DCentroid<RefPointType>(*temp_cloud, c);
-					int segment_index = 0;
-					double segment_distance = c[0] * c[0] + c[1] * c[1];
-					for (size_t i = 1; i < cluster_indices.size(); i++)
-					{
-						temp_cloud.reset(new Cloud);
-						extractSegmentCluster(target_cloud, cluster_indices, int(i), *temp_cloud);
-						pcl::compute3DCentroid<RefPointType>(*temp_cloud, c);
-						double distance = c[0] * c[0] + c[1] * c[1];
-						if (distance < segment_distance)
-						{
-							segment_index = int(i);
-							segment_distance = distance;
-						}
-					}
-
-					segmented_cloud_.reset(new Cloud);
-					extractSegmentCluster(target_cloud, cluster_indices, segment_index, *segmented_cloud_);
-					//pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-					//normalEstimation (segmented_cloud_, *normals);
-					RefCloudPtr ref_cloud(new RefCloud);
-					//addNormalToCloud (segmented_cloud_, normals, *ref_cloud);
-					ref_cloud = segmented_cloud_;
-					RefCloudPtr nonzero_ref(new RefCloud);
-					removeZeroPoints(ref_cloud, *nonzero_ref);
-
-					PCL_INFO("calculating cog\n");
-
-					RefCloudPtr transed_ref(new RefCloud);
-					pcl::compute3DCentroid<RefPointType>(*nonzero_ref, c);
-					Eigen::Affine3f trans = Eigen::Affine3f::Identity();
-					trans.translation().matrix() = Eigen::Vector3f(c[0], c[1], c[2]);
-					//pcl::transformPointCloudWithNormals<RefPointType> (*ref_cloud, *transed_ref, trans.inverse());
-					pcl::transformPointCloud<RefPointType>(*nonzero_ref, *transed_ref, trans.inverse());
-					CloudPtr transed_ref_downsampled(new Cloud);
-					gridSample(transed_ref, *transed_ref_downsampled, downsampling_grid_size_);
-					tracker_->setReferenceCloud(transed_ref_downsampled);
+#else
+					tracker_->setReferenceCloud(ref_cloud_);
+#endif
+					//Praveen
 					tracker_->setTrans(trans);
 					reference_ = transed_ref;
 					tracker_->setMinIndices(int(ref_cloud->points.size()) / 2);
@@ -779,12 +669,9 @@ public:
 	{
 		try{
 			pcl::Grabber* grabber = new pcl::Kinect2Grabber();
-			/*boost::function<void(const CloudConstPtr&)> f =
-				boost::bind(&OpenNISegmentTracking::cloud_cb, this, _1);*/
-			boost::function<void(const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZRGBA>> &)> f =
+			boost::function<void(const CloudConstPtr&)> f =
 				boost::bind(&OpenNISegmentTracking::cloud_cb, this, _1);
 			
-			//boost::signals2::connection ret = grabber->registerCallback(f);
 			grabber->registerCallback(f);
 
 			viewer_.runOnVisualizationThread(boost::bind(&OpenNISegmentTracking::viz_cb, this, _1), "viz_cb");
@@ -809,6 +696,7 @@ public:
 	CloudPtr cloud_hull_;
 	CloudPtr segmented_cloud_;
 	CloudPtr reference_;
+	CloudPtr ref_cloud_;
 	std::vector<pcl::Vertices> hull_vertices_;
 
 	std::string device_id_;
