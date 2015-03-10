2cd1fe43e54139ff81d1e50e014e4bbf67f31299
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-03-10 15:39:36 +0900

    Added gesture recognition application.
    Currently supports only Hand wave right and left gestures

---
+++ GestureRecognition/GestureCommon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
+{
+    static class GestureNames
+    {
+        public static readonly string None = "None";
+        public static readonly string HandwaveLeft = "Greet_Left";
+        public static readonly string HandwaveRight = "Greet_Right";
+    }
+}
