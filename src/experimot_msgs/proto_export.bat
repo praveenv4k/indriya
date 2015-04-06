c817eca950ab051552c8ab60d2439367d3b92428
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-06 17:00:19 +0900

    projects output folder configuration

--- src/experimot_msgs/proto_export.bat
+++ src/experimot_msgs/proto_export.bat
@@ -11,7 +11,7 @@ for /r %%i in (.\proto\*.proto) do %1 --python_out=%2\python --proto_path=%4 %%i
 REM Csharp Files
 REM Descriptor File
 REM for /r %%i in (proto\*.proto) do %1 --descriptor_set_out=%2descriptor\%%~nxi --proto_path=%4 %%i
-for /r %%i in (proto\*.proto) do %5 -o:%2csharp\%%~ni.cs -i:%%i
+for /r %%i in (proto\*.proto) do %5 -o:%2csharp\%%~ni.cs -i:%%i -ns:Experimot.Core.Msgs
 
 REM Some random tries
 REM for /r %%i in (.\proto_kinect\*.proto) do %1 --cpp_out=%2 --proto_path=%4_kinect %%i
