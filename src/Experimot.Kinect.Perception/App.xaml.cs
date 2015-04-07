75a3b49d8ce71a4972fb24f10bd3af90f4429ac5
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-07 21:03:13 +0900

    check parameter server connection in c# application

--- src/Experimot.Kinect.Perception/App.xaml.cs
+++ src/Experimot.Kinect.Perception/App.xaml.cs
@@ -1,17 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
+using CommandLine;
+using experimot.msgs;
+using ProtoBuf;
+using ZMQ;
+using Exception = System.Exception;
 
-namespace ExperimotPerception
+namespace Experimot.Kinect.Perception
 {
+    class Options
+    {
+        [Option('n', "name", DefaultValue = "gesture_recognition", HelpText = "This is the name of the node!")]
+        public string Name { get; set; }
+        [Option('p', "param", DefaultValue = "tcp://localhost:5560", HelpText = "This is the address of parameter server!")]
+        public string ParameterServer { get; set; }
+    }
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var args = e.Args;
+            var options = new Options();
+            Parser.Default.ParseArguments(args, options);
+            var info = GetNodeInfo(options.Name, options.ParameterServer);
+            if (info != null)
+            {
+            }
+            base.OnStartup(e);
+        }
+
+        private Node GetNodeInfo(string name, string server, int timeout=1000)
+        {
+            try
+            {
+                using (var context = new Context(1))
+                {
+                    using (var socket = context.Socket(SocketType.REQ))
+                    {
+                        socket.Connect(server);
+                        if (socket.Send(name, Encoding.ASCII) == SendStatus.Sent)
+                        {
+                            byte[] msg = socket.Recv(timeout);
+                            if (msg != null)
+                            {
+                                using (var memStream = new MemoryStream(msg))
+                                {
+                                    var nodeInfo = Serializer.Deserialize<Node>(memStream);
+                                    return nodeInfo;
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show(string.Format("Message buffer empty!"));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+
     }
 }
