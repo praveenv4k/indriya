ebb5ff56e34a5c73639659bd23ba6d66fe3c2bb1
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-10 16:30:47 +0900

    adding ekf filter for filtering the pose of the torso

---
+++ src/experimot_localization/TorsoPoseFilter.cpp
@@ -0,0 +1,180 @@
+#include "TorsoPoseFilter.h"
+
+//#define B3_FORCE_INLINE 1
+//#define B3_USE_DOUBLE_PRECISION
+#include <bullet\Bullet3Common\b3Matrix3x3.h>
+
+using namespace MatrixWrapper;
+using namespace BFL;
+using namespace std;
+
+TorsoPoseFilter::TorsoPoseFilter() :prior_(NULL),
+filter_(NULL),
+filter_initialized_(false),
+vo_initialized_(false)
+{
+	// create SYSTEM MODEL
+	ColumnVector sysNoise_Mu(6);  sysNoise_Mu = 0;
+	SymmetricMatrix sysNoise_Cov(6); sysNoise_Cov = 0;
+	for (unsigned int i = 1; i <= 6; i++) sysNoise_Cov(i, i) = pow(1000.0, 2);
+	Gaussian system_Uncertainty(sysNoise_Mu, sysNoise_Cov);
+	sys_pdf_ = new NonLinearAnalyticConditionalGaussianOdo(system_Uncertainty);
+	sys_model_ = new AnalyticSystemModelGaussianUncertainty(sys_pdf_);
+
+	// create MEASUREMENT MODEL VO
+	ColumnVector measNoiseVo_Mu(6);  measNoiseVo_Mu = 0;
+	SymmetricMatrix measNoiseVo_Cov(6);  measNoiseVo_Cov = 0;
+	for (unsigned int i = 1; i <= 6; i++) measNoiseVo_Cov(i, i) = 1;
+	Gaussian measurement_Uncertainty_Vo(measNoiseVo_Mu, measNoiseVo_Cov);
+	Matrix Hvo(6, 6);  Hvo = 0;
+	Hvo(1, 1) = 1;    Hvo(2, 2) = 1;    Hvo(3, 3) = 1;    Hvo(4, 4) = 1;    Hvo(5, 5) = 1;    Hvo(6, 6) = 1;
+	vo_meas_pdf_ = new LinearAnalyticConditionalGaussian(Hvo, measurement_Uncertainty_Vo);
+	vo_meas_model_ = new LinearAnalyticMeasurementModelGaussianUncertainty(vo_meas_pdf_);
+}
+
+TorsoPoseFilter::~TorsoPoseFilter(){
+	if (filter_) delete filter_;
+	if (prior_)  delete prior_;
+	delete vo_meas_model_;
+	delete vo_meas_pdf_;
+	delete sys_pdf_;
+	delete sys_model_;
+}
+
+/** update the extended Kalman filter **/
+bool TorsoPoseFilter::update(const PosixTime& filter_time){
+	// only update filter when it is initialized
+	if (!filter_initialized_){
+		//ROS_INFO("Cannot update filter when filter was not initialized first.");
+		return false;
+	}
+
+	// only update filter for time later than current filter time
+	double dt = (filter_time - filter_time_old_).seconds();
+	if (dt == 0) return false;
+	if (dt < 0){
+		//ROS_INFO("Will not update robot pose with time %f sec in the past.", dt);
+		return false;
+	}
+	//ROS_DEBUG("Update filter at time %f with dt %f", filter_time.toSec(), dt);
+
+
+	// system update filter
+	// --------------------
+	// for now only add system noise
+	ColumnVector vel_desi(2); vel_desi = 0;
+	filter_->Update(sys_model_, vel_desi);
+
+	// process vo measurement
+	// ----------------------
+	if (vo_initialized_){
+		// convert absolute vo measurements to relative vo measurements
+		Transform vo_rel_frame = filter_estimate_old_ * vo_meas_old_.inverse() * vo_meas_;
+		ColumnVector vo_rel(6);
+		decomposeTransform(vo_rel_frame, vo_rel(1), vo_rel(2), vo_rel(3), vo_rel(4), vo_rel(5), vo_rel(6));
+		angleOverflowCorrect(vo_rel(6), filter_estimate_old_vec_(6));
+		// update filter
+		vo_meas_pdf_->AdditiveNoiseSigmaSet(vo_covariance_ * pow(dt, 2));
+		filter_->Update(vo_meas_model_, vo_rel);
+	}
+	else{
+		vo_initialized_ = true;
+	}
+
+	vo_meas_old_ = vo_meas_;
+
+	// remember last estimate
+	filter_estimate_old_vec_ = filter_->PostGet()->ExpectedValueGet();
+
+	b3Quaternion q;
+	q.setEulerZYX(filter_estimate_old_vec_(6), filter_estimate_old_vec_(5), filter_estimate_old_vec_(4));
+
+	filter_estimate_old_ = Transform(Vector(q.w, q.x, q.y, q.z),
+		Vector(filter_estimate_old_vec_(1), filter_estimate_old_vec_(2), filter_estimate_old_vec_(3)));
+
+	filter_time_old_ = filter_time;
+
+	addMeasurement(filter_estimate_old_);
+
+	return true;
+}
+
+/** initialize the extended Kalman filter **/
+void TorsoPoseFilter::initialize(const OpenRAVE::Transform& prior, const PosixTime& time){
+	// set prior of filter
+	ColumnVector prior_Mu(6);
+	decomposeTransform(prior, prior_Mu(1), prior_Mu(2), prior_Mu(3), prior_Mu(4), prior_Mu(5), prior_Mu(6));
+	SymmetricMatrix prior_Cov(6);
+	for (unsigned int i = 1; i <= 6; i++) {
+		for (unsigned int j = 1; j <= 6; j++){
+			if (i == j) {
+				prior_Cov(i, j) = pow(0.001, 2);
+			}
+			else{
+				prior_Cov(i, j) = 0;
+			}
+		}
+	}
+	prior_ = new Gaussian(prior_Mu, prior_Cov);
+	filter_ = new ExtendedKalmanFilter(prior_);
+
+	// remember prior
+	addMeasurement(prior);
+
+	filter_estimate_old_vec_ = prior_Mu;
+	filter_estimate_old_ = prior;
+	filter_time_old_ = time;
+
+	// filter initialized
+	filter_initialized_ = true;
+}
+
+/** get the filter posterior **/
+void TorsoPoseFilter::getEstimate(MatrixWrapper::ColumnVector& estimate){
+	estimate = filter_estimate_old_vec_;
+}
+
+/** get the filter posterior **/
+void TorsoPoseFilter::getEstimate(OpenRAVE::Transform& estimate){
+	estimate = filter_estimate_old_;
+}
+
+/** Add a sensor measurement to the measurement buffer **/
+void TorsoPoseFilter::addMeasurement(const OpenRAVE::Transform& meas){
+	vo_meas_ = meas;
+}
+
+/** Add a sensor measurement to the measurement buffer **/
+void TorsoPoseFilter::addMeasurement(const OpenRAVE::Transform& meas, const MatrixWrapper::SymmetricMatrix& covar){
+	// check covariance
+	for (unsigned int i = 0; i<covar.rows(); i++){
+		if (covar(i + 1, i + 1) == 0){
+			//ROS_ERROR("Covariance specified for measurement on topic %s is zero", meas.child_frame_id_.c_str());
+			return;
+		}
+	}
+	// add measurements
+	addMeasurement(meas);
+	vo_covariance_ = covar;
+	//else ROS_ERROR("Adding a measurement for an unknown sensor %s", meas.child_frame_id_.c_str());
+}
+
+/// correct for angle overflow
+void TorsoPoseFilter::angleOverflowCorrect(double& a, double ref){
+	while ((a - ref) >  M_PI) a -= 2 * M_PI;
+	while ((a - ref) < -M_PI) a += 2 * M_PI;
+}
+
+// decompose Transform into x,y,z,Rx,Ry,Rz
+void TorsoPoseFilter::decomposeTransform(const OpenRAVE::Transform& trans,
+	double& x, double& y, double&z, double&Rx, double& Ry, double& Rz){
+	Vector quat = trans.rot;
+	b3Quaternion b3quat(quat[1], quat[2], quat[3], quat[0]);
+	x = trans.trans.x;
+	y = trans.trans.y;
+	z = trans.trans.z;
+	b3Matrix3x3 b3Mat(b3quat);
+	float roll, pitch, yaw;
+	b3Mat.getEulerYPR(yaw, pitch, roll);
+	Rx = roll; Ry = pitch; Rz = yaw;
+}
---
+++ src/experimot_localization/TorsoPoseFilter.h
@@ -0,0 +1,106 @@
+#ifndef __TORSO_POSE_FILTER_H__
+#define __TORSO_POSE_FILTER_H__
+
+#include <bfl\filter\extendedkalmanfilter.h>
+#include <bfl/wrappers/matrix/matrix_wrapper.h>
+#include <bfl/model/linearanalyticsystemmodel_gaussianuncertainty.h>
+#include <bfl/model/linearanalyticmeasurementmodel_gaussianuncertainty.h>
+#include <bfl/pdf/analyticconditionalgaussian.h>
+#include <bfl/pdf/linearanalyticconditionalgaussian.h>
+#include "nonlinearanalyticconditionalgaussianodo.h"
+
+#include <openrave-core.h>
+
+typedef boost::posix_time::ptime PosixTime;
+typedef boost::posix_time::time_duration PosixDuration;
+
+using namespace OpenRAVE;
+
+class TorsoPoseFilter
+{
+public:
+	TorsoPoseFilter();
+	~TorsoPoseFilter();
+
+	/** update the extended Kalman filter
+	* \param odom_active specifies if the odometry sensor is active or not
+	* \param imu_active specifies if the imu sensor is active or not
+	* \param gps_active specifies if the gps sensor is active or not
+	* \param vo_active specifies if the vo sensor is active or not
+	* \param filter_time update the ekf up to this time
+	* \param diagnostics_res returns false if the diagnostics found that the sensor measurements are inconsistent
+	* returns true on successfull update
+	*/
+	//bool update(bool odom_active, bool imu_active, bool gps_active, bool vo_active, const PosixTime& filter_time, bool& diagnostics_res);
+	bool update(const PosixTime& filter_time);
+
+	/** initialize the extended Kalman filter
+	* \param prior the prior robot pose
+	* \param time the initial time of the ekf
+	*/
+	void initialize(const OpenRAVE::Transform& prior, const PosixTime& time);
+
+	/** check if the filter is initialized
+	* returns true if the ekf has been initialized already
+	*/
+	bool isInitialized() { return filter_initialized_; };
+
+	/** get the filter posterior
+	* \param estimate the filter posterior as a columnvector
+	*/
+	void getEstimate(MatrixWrapper::ColumnVector& estimate);
+
+	/** get the filter posterior
+	* \param estimate the filter posterior as a pose with covariance
+	*/
+	void getEstimate(OpenRAVE::Transform& estimate);
+
+	/** Add a sensor measurement to the measurement buffer
+	* \param meas the measurement to add
+	*/
+	void addMeasurement(const OpenRAVE::Transform& meas);
+
+	/** Add a sensor measurement to the measurement buffer
+	* \param meas the measurement to add
+	* \param covar the 6x6 covariance matrix of this measurement, as defined in the PoseWithCovariance message
+	*/
+	void addMeasurement(const OpenRAVE::Transform& meas, const MatrixWrapper::SymmetricMatrix& covar);
+
+	/** set the output frame used by tf
+	* \param output_frame the desired output frame published on /tf (e.g., odom_combined)
+	*/
+	void setOutputFrame(const std::string& output_frame);
+
+	/** set the base_footprint frame of the robot used by tf
+	* \param base_frame the desired base frame from which to transform when publishing the combined odometry frame (e.g., base_footprint)
+	*/
+	void setBaseFootprintFrame(const std::string& base_frame);
+
+private:
+	/// correct for angle overflow
+	void angleOverflowCorrect(double& a, double ref);
+
+	// decompose Transform into x,y,z,Rx,Ry,Rz
+	void decomposeTransform(const OpenRAVE::Transform& trans,
+		double& x, double& y, double&z, double&Rx, double& Ry, double& Rz);
+
+private:
+	// pdf / model / filter
+	BFL::AnalyticSystemModelGaussianUncertainty*            sys_model_;
+	BFL::NonLinearAnalyticConditionalGaussianOdo*           sys_pdf_;
+	BFL::LinearAnalyticConditionalGaussian*                 vo_meas_pdf_;
+	BFL::LinearAnalyticMeasurementModelGaussianUncertainty* vo_meas_model_;
+	BFL::Gaussian*                                          prior_;
+	BFL::ExtendedKalmanFilter*                              filter_;
+	MatrixWrapper::SymmetricMatrix                          vo_covariance_;
+
+	// vars
+	MatrixWrapper::ColumnVector vel_desi_, filter_estimate_old_vec_;
+	OpenRAVE::Transform filter_estimate_old_;
+	OpenRAVE::Transform vo_meas_, vo_meas_old_;
+	PosixTime filter_time_old_;
+	bool filter_initialized_, vo_initialized_;
+
+};
+
+#endif
