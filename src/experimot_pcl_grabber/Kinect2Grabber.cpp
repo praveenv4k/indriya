d615b376ebe3a529ec18b0adeabdfe59949485ab
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-13 19:09:39 +0900

    export kinect2 grabber header files

---
+++ src/experimot_pcl_grabber/Kinect2Grabber.cpp
@@ -0,0 +1,1 @@
+#include "Kinect2Grabber.hpp"
---
+++ src/experimot_pcl_grabber/Kinect2_PCL.h
@@ -0,0 +1,5 @@
+// Automatically generated! DO NOT EDIT!!! 
+// Experimot Message Header Files 
+// Created on  
+// Mon 04/13/2015 19:08:11.83  
+#include "Kinect2Grabber.hpp"  
