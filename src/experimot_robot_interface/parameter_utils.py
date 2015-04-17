d4ca5edd640f034867336d1771ec0b6485ab954b
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-18 02:15:02 +0900

    nao behavior manager bug fixes

--- src/experimot_robot_interface/parameter_utils.py
+++ src/experimot_robot_interface/parameter_utils.py
@@ -1,6 +1,7 @@
 import socket
 import zmq
 import node_pb2
+import sys
 
 
 def getNodeParameters(name,parameterServerAddress,timeout):
@@ -9,6 +10,21 @@ def getNodeParameters(name,parameterServerAddress,timeout):
         socket = context.socket(zmq.REQ)
         socket.connect(parameterServerAddress)
         socket.send(name)
-        
+        str = socket.recv()
+        #str = socket.recv_string(encoding='latin1')
+        #print str
+        node = node_pb2.Node()
+        node.ParseFromString(str)
+        socket.close()
+        print node
+        return node
     except:
         print "getNodeParameters : ", sys.exc_info()
+
+def getParam(node,key,defaultValue):
+    ret = defaultValue
+    if node != None:
+        for p in node.param:
+            if p.key == key:
+                ret = p.value
+    return ret
