f86a7cf7fa96a3a2742eb8dbdd5ca6c9e59c6fa4
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-18 15:35:34 +0900

    fixed bug in the behavior manager

--- src/experimot_robot_interface/parameter_utils.py
+++ src/experimot_robot_interface/parameter_utils.py
@@ -1,30 +1,66 @@
 import socket
 import zmq
 import node_pb2
+import robot_behavior_pb2
 import sys
-
 
 def getNodeParameters(name,parameterServerAddress,timeout):
     try:
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(parameterServerAddress)
-        socket.send(name)
-        str = socket.recv()
-        #str = socket.recv_string(encoding='latin1')
-        #print str
-        node = node_pb2.Node()
-        node.ParseFromString(str)
-        socket.close()
-        print node
-        return node
+        # using with keyword save the life! If not used, the unmanaged resources are getting cleaned up
+        # and this caused problems with interacting with Naoqi proxy
+        with zmq.Context.instance() as ctx: 
+            with ctx.socket(zmq.REQ) as sock:
+                #socket = context.socket(zmq.REQ)
+                sock.connect(parameterServerAddress)
+                print "Sending parameter retrieval request"
+                sock.send(name)
+                print "waiting parameter response"
+                str = sock.recv()
+                node = node_pb2.Node()
+                print "parsing parameter response"
+                node.ParseFromString(str)
+                sock.close()
+                print node
+                return node
     except:
         print "getNodeParameters : ", sys.exc_info()
+
 
 def getParam(node,key,defaultValue):
     ret = defaultValue
     if node != None:
         for p in node.param:
             if p.key == key:
-                ret = p.value
+                ret = p.value.encode('utf-8') # needed to encode in utf-8 because naoqi doesn't accept unicode
     return ret
+
+def register_motions(name,parameterServerAddress,motions):
+    print "Creating behavior module message"
+    behaviorModule = robot_behavior_pb2.RobotBehaviorModule()
+    behaviorModule.name = name
+
+    for motion in motions:
+        print "Creating behavior description message", motion
+        desc = behaviorModule.behaviors.add()
+        desc.name = motion
+        desc.type = robot_behavior_pb2.BehaviorDescription.Blocking
+        desc.state = robot_behavior_pb2.BehaviorDescription.Idle
+
+    print behaviorModule
+
+    try:
+        with zmq.Context.instance() as ctx: 
+            with ctx.socket(zmq.REQ) as sock:
+                parts = []
+                str = behaviorModule.SerializeToString();
+                sock.connect(parameterServerAddress)
+                parts.append("register_behaviors")
+                parts.append(name.encode('utf-8'))
+                parts.append(str)                
+                print "Sending register_motions message", parts
+                sock.send_multipart(parts)
+                response = sock.recv()
+                sock.close()
+                print response
+    except:
+        print "register_motions : ", sys.exc_info()
--- src/experimot_robot_interface/test_dir.py
+++ src/experimot_robot_interface/test_dir.py
@@ -4,9 +4,25 @@ import time
 import argparse
 
 from naoqi import ALProxy
+import robot_behavior_pb2
 
 from os.path import dirname
 from os.path import abspath
+
+def register_motions(name,parameterServerAddress,motions):
+    behaviorModule = robot_behavior_pb2.RobotBehaviorModule()
+    behaviorModule.name = name
+
+    for motion in motions:
+        desc = behaviorModule.behaviors.add()
+        desc.name = motion
+        desc.type = robot_behavior_pb2.BehaviorDescription.Blocking
+        desc.state = robot_behavior_pb2.BehaviorDescription.Idle
+
+    print behaviorModule
+
+
+register_motions("hello","",["move","sit","stand"])
 
 currdir = dirname(__file__)
 parent = abspath(os.path.join(currdir,os.pardir))
@@ -16,6 +32,8 @@ i = 0
 bin = os.environ["DEV_SDK_ROOT"]
 
 print bin
+
+
 
 parent = currdir
 while i<4:
