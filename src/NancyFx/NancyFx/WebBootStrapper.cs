e182b1c241f8c6c3bdbceb83d35eea372eb5ffde
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-22 19:33:15 +0900

    Nancy web framework example

---
+++ src/NancyFx/NancyFx/WebBootStrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+
+namespace NancyFx
+{
+    class WebBootStrapper: DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("www"));
+        }
+    }
+}
