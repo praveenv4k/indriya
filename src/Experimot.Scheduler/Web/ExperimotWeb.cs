feaf4aae4a067955adae2b0a3b71e781de637532
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-04-23 17:52:01 +0900

    web integration

--- src/Experimot.Scheduler/Web/ExperimotWeb.cs
+++ src/Experimot.Scheduler/Web/ExperimotWeb.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Common.Logging;
 using Experimot.Core;
+using Experimot.Core.Util;
+using Nancy.Hosting.Self;
+using NancyFx;
 
 namespace Experimot.Scheduler.Web
 {
-    class ExperimotWeb: IDisposable
+    internal class ExperimotWeb : IDisposable
     {
+        private readonly experimot_config _config;
         private bool _disposed;
         private static readonly ILog Log = LogManager.GetLogger(typeof (ExperimotWeb));
-        private readonly Nancy.Hosting.Self.NancyHost _host;
+        private NancyHost _host;
 
         public ExperimotWeb(experimot_config config)
         {
-            
+            _config = config;
         }
 
         public void Dispose()
@@ -27,13 +27,33 @@ namespace Experimot.Scheduler.Web
 
         public void Start()
         {
-            
+            if (_config != null && _config.parameters != null && _config.parameters.Count > 0 && _host == null)
+            {
+                var enabled = ParameterUtil.Get(_config.parameters, "WebServerEnabled", false);
+                if (enabled)
+                {
+                    var host = ParameterUtil.Get(_config.parameters, "WebServerHost", "http://localhost");
+                    var port = ParameterUtil.Get(_config.parameters, "WebServerPort", 8888);
+
+                    var uriStr = string.Format("{0}:{1}", host, port);
+                    _host = new NancyHost(new Uri(uriStr), new ExperimotWebBootStrapper());
+
+                    _host.Start();
+
+                    Log.InfoFormat("Experimot web interface started at {0}", uriStr);
+                }
+            }
         }
 
         public void Stop()
         {
-            
-
+            if (_host != null)
+            {
+                _host.Stop();
+                _host.Dispose();
+                _host = null;
+                Log.Info("Experimot web interface stopped");
+            }
         }
 
         // Protected implementation of Dispose pattern. 
--- src/Experimot.Scheduler/Web/ExperimotWebBootStrapper.cs
+++ src/Experimot.Scheduler/Web/ExperimotWebBootStrapper.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nancy;
+﻿using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace NancyFx
 {
-    class WebBootStrapper: DefaultNancyBootstrapper
+    class ExperimotWebBootStrapper: DefaultNancyBootstrapper
     {
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
