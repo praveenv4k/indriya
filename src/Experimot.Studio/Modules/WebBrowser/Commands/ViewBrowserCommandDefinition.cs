0402ec3a5a985d746ec597c6be4df11d3e81ce8d
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-01 02:05:10 +0900

    refactor studio app

---
+++ src/Experimot.Studio/Modules/WebBrowser/Commands/ViewBrowserCommandDefinition.cs
@@ -0,0 +1,25 @@
+﻿using Gemini.Framework.Commands;
+
+namespace Experimot.Studio.Modules.WebBrowser.Commands
+{
+    [CommandDefinition]
+    public class ViewBrowserCommandDefinition : CommandDefinition
+    {
+        public const string CommandName = "View.Home";
+
+        public override string Name
+        {
+            get { return CommandName; }
+        }
+
+        public override string Text
+        {
+            get { return "Home"; }
+        }
+
+        public override string ToolTip
+        {
+            get { return "Home"; }
+        }
+    }
+}
---
+++ src/Experimot.Studio/Modules/WebBrowser/Commands/ViewBrowserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Experimot.Studio.Modules.WebBrowser.ViewModels;
+using Gemini.Framework;
+using Gemini.Framework.Commands;
+using Gemini.Framework.Services;
+using Gemini.Framework.Threading;
+
+namespace Experimot.Studio.Modules.WebBrowser.Commands
+{
+    [CommandHandler]
+    public class ViewBrowserCommandHandler : CommandHandlerBase<ViewBrowserCommandDefinition>
+    {
+        private readonly IShell _shell;
+
+        [ImportingConstructor]
+        public ViewBrowserCommandHandler(IShell shell)
+        {
+            _shell = shell;
+        }
+
+        public override Task Run(Command command)
+        {
+            _shell.OpenDocument((IDocument) IoC.GetInstance(typeof(WebBrowserViewModel), null));
+            return TaskUtility.Completed;
+        }
+    }
+}
