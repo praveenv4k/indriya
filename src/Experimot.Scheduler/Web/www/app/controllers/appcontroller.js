80ee22555b738833919c6f3a1e250d9bc0e20e07
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-01 19:10:32 +0900

    backbone marionette app structure

--- src/Experimot.Scheduler/Web/www/app/controllers/appcontroller.js
+++ src/Experimot.Scheduler/Web/www/app/controllers/appcontroller.js
@@ -1,1 +1,13 @@
-﻿
+﻿define(['app', 'backbone', 'marionette', 'views/main'],
+    function (app, backbone, marionette, main) {
+        return backbone.Marionette.Controller.extend({
+            initialize: function (options) {
+                console.log(options);
+                //App.headerRegion.show(new DesktopHeaderView());
+            },
+            //gets mapped to in AppRouter's appRoutes
+            index: function () {
+                app.mainRegion.show(new main());
+            }
+        });
+    });
