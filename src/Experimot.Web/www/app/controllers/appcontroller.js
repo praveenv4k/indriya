60009f176969b21771e8f1e2223788cca759c900
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-04 13:50:56 +0900

    poller added.

--- src/Experimot.Web/www/app/controllers/appcontroller.js
+++ src/Experimot.Web/www/app/controllers/appcontroller.js
@@ -1,5 +1,5 @@
-﻿define(['app', 'backbone', 'marionette', 'collections/drawables', 'marionette_threejs'],
-    function (app, backbone, marionette, drawables, m3Js) {
+﻿define(['app', 'backbone', 'marionette', 'models/robot', 'collections/drawables', 'marionette_threejs', 'poller'],
+    function (app, backbone, marionette, robot, drawables, m3Js,poller) {
         return backbone.Marionette.Controller.extend({
             initialize: function (options) {
                 app.Drawables = new drawables();
@@ -7,17 +7,22 @@
                     collection: app.Drawables
                 });
 
-                
+
                 app.loader = new THREE.ColladaLoader();
                 app.loader.options.convertUpAxis = true;
-                //var globalCh = Backbone.Wreqr.radio.channel('global');
-                //App.StatusPoller = Poller.get(App.MachineStatus, this.pollerOptions());
-                //App.StatusPoller.on('error', function (model) {
-                //    globalCh.vent.trigger("serverCommunication", false);
-                //    console.error('Error retrieving machine Status information from server!');
-                //});
-                //App.StatusPoller.start();
+
+                var globalCh = Backbone.Wreqr.radio.channel('global');
+
+                app.robot = new robot();
+
+                app.robotpoller = poller.get(app.robot, this.pollerOptions());
+                app.robotpoller.on('error', function (model) {
+                    globalCh.vent.trigger("serverCommunication", false);
+                    console.error('Error retrieving machine Status information from server!');
+                });
+                app.robotpoller.start();
             },
+
             index: function() {
                 require(["jquery", "app", "views/designer", "views/monitor"],
                     function($, app, designer, monitor) {
@@ -74,6 +79,30 @@
 
                 var newDrawable = new m3Js.Drawable(options);
                 app.Drawables.add(newDrawable);
+            },
+
+            pollerOptions: function () {
+                var options = {
+
+                    // default delay is 1000ms
+                    delay: 100,
+
+                    // run after a delayed interval. defaults to false
+                    // can be a boolean `true` to wait `delay` ms before starting or a number to override the wait period
+                    delayed: 1000,
+
+                    // do not stop the poller on error. defaults to false
+                    // `error` event is always fired even with this option on.
+                    continueOnError: false,
+
+                    // condition for keeping polling active (when this stops being true, polling will stop)
+                    //condition: function (model) {
+                    //    return model.get('active') === true;
+                    //},
+
+                    // We can pass data to a fetch request
+                    //data: { fields: "*", sort: "name asc" }
+                }
             }
         });
     });
