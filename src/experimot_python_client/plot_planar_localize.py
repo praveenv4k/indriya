b24e3e758e0307099f13482b18b52bb734828c1a
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-12 09:51:24 +0900

    plot planar pose

--- src/experimot_python_client/plot_planar_localize.py
+++ src/experimot_python_client/plot_planar_localize.py
@@ -7,18 +7,29 @@ import matplotlib.patches as patches
 import matplotlib.animation as animation
 import numpy as np
 
-def csv_reader(file_obj):
+def csv_plot(file_obj):
     reader = csv.reader(file_obj)
     data = []
+    
     i=0
+    x_val = []
+    y_val = []
+    theta_val = []
     for row in reader:
         
         if i>0:
             x = np.array(row)
             y = x.astype(np.float)
             data.append(y)
+            x_val.append(row[1])
+            y_val.append(row[2])
+            theta_val.append(row[3])
         i=i+1
         #time.sleep(0.1)
+    plt.plot(x_val)
+    plt.plot(y_val)
+    plt.plot(theta_val)
+    plt.show()
     return data
 
 if __name__ == "__main__":
@@ -27,7 +38,7 @@ if __name__ == "__main__":
 
     csv_path = "planar_right_turn.csv"
     with open(csv_path, "rb") as f_obj:
-        data = csv_reader(f_obj)
-        
-        plt.plot(data)
-        plt.show()
+        data = csv_plot(f_obj)
+        #x = data[:,2]
+        #plt.plot(data)
+        #plt.show()
