3f8d7a1dd7969990864131abb1883f8ecb962b48
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-22 14:20:50 +0900

    tried with bower

---
+++ src/Experimot.Web/www/require_config.js
@@ -0,0 +1,17 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    backbone: "bower_components/backbone/backbone",
+    "backbone-modal": "bower_components/backbone-modal/backbone.modal",
+    "backbone-poller": "bower_components/backbone-poller/backbone.poller",
+    "jquery-jsonview": "bower_components/jquery-jsonview/dist/jquery.jsonview",
+    marionette: "bower_components/marionette/lib/core/backbone.marionette",
+    "marionette-threejs": "bower_components/marionette-threejs/marionette-threejs",
+    requirejs: "bower_components/requirejs/require"
+  },
+  packages: [
+
+  ]
+});
