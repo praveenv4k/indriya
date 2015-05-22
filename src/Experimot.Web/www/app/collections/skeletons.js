eb80c812ce99cb4f8e04ef2419a26405ef14e733
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-22 19:41:17 +0900

    paper modification.

---
+++ src/Experimot.Web/www/app/collections/skeletons.js
@@ -0,0 +1,16 @@
+﻿// Model.js
+// --------
+define(["jquery", "backbone", "models/skeleton"],
+    function($, backbone, model) {
+
+        // Creates a new Backbone Model class object
+        var programs = backbone.Collection.extend({
+            model: model,
+            url: '/visualize/skeleton/list'
+        });
+
+        // Returns the Model class
+        return programs;
+
+    }
+);
