6cf8d1dd6473a60b95491ff9aadfa7d4a2943e9c
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-05-22 13:31:55 +0900

    refactoring

--- src/Experimot.Scheduler/MainWindow.xaml.cs
+++ src/Experimot.Scheduler/MainWindow.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using Common.Logging;
-using experimot.msgs;
 using Experimot.Scheduler.Annotations;
-using Experimot.Scheduler.Data;
+using Experimot.Scheduler.Mvvm;
 using Nancy.TinyIoc;
 
 namespace Experimot.Scheduler
@@ -13,42 +10,14 @@ namespace Experimot.Scheduler
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof (MainWindow));
-
         public MainWindow()
         {
-            const string configFile = "experimot_config.xml";
-            if (!string.IsNullOrEmpty(configFile))
-            {
-                _bootStrapper = new BootStrapper(configFile);
-                _bootStrapper.StartUp();
-            }
-            DataContext = Context;
+            DataContext = new MainWindowViewModel();
             InitializeComponent();
-            Closing += MainWindow_Closing;
         }
-
-        public Context Context
-        {
-            get
-            {
-                var ctx = TinyIoCContainer.Current.Resolve<Context>();
-                return ctx;
-            }
-        }
-
-        private void MainWindow_Closing(object sender, CancelEventArgs e)
-        {
-            if (_bootStrapper != null)
-            {
-                _bootStrapper.Shutdown();
-            }
-        }
-
-        private readonly BootStrapper _bootStrapper;
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
@@ -56,18 +25,6 @@ namespace Experimot.Scheduler
         {
             var handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private int prev = 7;
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (Context != null)
-            {
-                Context.Humans.Add(new Data.Human(prev, Context.MotionModules)
-                {
-                    Body = new KinectBody() { TrackingId = prev++}
-                });
-            }
         }
 
         private void ProgramExecution(ExecutionRequest req)
---
+++ src/Experimot.Scheduler/Mvvm/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Nancy.TinyIoc;
+
+namespace Experimot.Scheduler.Mvvm
+{
+    public class MainWindowViewModel
+    {
+        private BrowserTabViewModel _browserTabViewModel;
+
+        public MainWindowViewModel()
+        {
+            TabViewModel = new BrowserTabViewModel("http://localhost:8888");
+        }
+
+        public BrowserTabViewModel TabViewModel
+        {
+            get { return _browserTabViewModel; }
+            set { _browserTabViewModel = value; }
+        }
+
+        public Context Context
+        {
+            get { return TinyIoCContainer.Current.Resolve<Context>(); }
+        }
+    }
+}
