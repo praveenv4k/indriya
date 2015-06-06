56d2105ae6c2ef7bb7c446ab425c02383100760b
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-06-06 16:36:50 +0900

    human detected behavior

--- src/Experimot.Web/www/libs/google/blockly/blocks/behavior.js
+++ src/Experimot.Web/www/libs/google/blockly/blocks/behavior.js
@@ -81,6 +81,7 @@ Blockly.Blocks.behaviors.rename = function(text) {
 
 Blockly.Blocks.behaviors.GestureProperties =
 [
+    ["HumanDetected", 'HumanDetected'],
     ["WaveLeft", 'Greet_Left'],
     ["WaveRight", 'Greet_Right'],
     ["LiftLeftHand", "LiftHand_Left"],
@@ -382,6 +383,130 @@ Blockly.Blocks['behavior_sleek'] = {
 };
 
 Blockly.Blocks['behavior_simple'] = {
+    init: function() {
+        var thisBlock = this;
+        var nameText = "behavior";
+        var name = Blockly.Blocks.behaviors.findLegalName(nameText, this);
+        var nameField = new Blockly.FieldTextInput(name, Blockly.Blocks.behaviors.rename);
+
+        var priorityDropdown = new Blockly.FieldDropdown(Blockly.Blocks.behaviors.PriorityProperties, function(option) {
+
+        });
+
+        var executionDropdown = new Blockly.FieldDropdown(Blockly.Blocks.behaviors.ExecutionProperties, function(option) {
+            var runUntilInput = (option == 'until');
+            this.sourceBlock_.updateShape_(runUntilInput);
+        });
+
+        var gestureDropdown = new Blockly.FieldDropdown(Blockly.Blocks.behaviors.GestureProperties, function(option) {
+            var kvp = Blockly.Blocks.behaviors.GestureProperties.filter(function(gesture) { return gesture[1] === option });
+            if (kvp.length > 0) {
+                thisBlock.setFieldValue(kvp[0][0] + 'Count', 'trigger_count');
+            }
+        });
+
+        this.setHelpUrl('http://www.example.com/');
+        this.setColour(330);
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_CENTRE)
+            .appendField("Behavior Name : ")
+            .appendField(nameField, "behavior_name");
+        this.appendDummyInput()
+            .appendField("");
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_LEFT)
+            .appendField("Human Motion : ");
+        this.appendDummyInput()
+            .appendField("When trigger is")
+            .appendField(gestureDropdown, "triggers");
+        this.appendDummyInput()
+            .appendField("")
+            .appendField("Robot Actions : ");
+        this.appendStatementInput("DO")
+            .appendField("do");
+        this.appendDummyInput()
+            .appendField("");
+
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_LEFT)
+            .appendField("Execution Settings : ");
+        this.appendDummyInput()
+            .appendField("With priority ")
+            .appendField(priorityDropdown, "priorities")
+            .appendField(" execute ")
+            .appendField(executionDropdown, "execution");
+        this.setTooltip('');
+    },
+
+    /**
+       * Return the signature of this behavior definition.
+       * @return {!Array} Tuple containing Behavior signature:
+       *     - the name of the defined procedure,
+       * @this Blockly.Block
+       */
+    getBehaviorDef: function() {
+        return [this.getFieldValue('behavior_name')];
+    },
+    renameProcedure: function(oldName, newName) {
+        if (Blockly.Names.equals(oldName, this.getBehaviorCall())) {
+            this.setFieldValue(newName, 'behavior_name');
+            this.setTooltip(
+                (this.outputConnection ? Blockly.Msg.PROCEDURES_CALLRETURN_TOOLTIP :
+                    Blockly.Msg.PROCEDURES_CALLNORETURN_TOOLTIP)
+                .replace('%1', newName));
+        }
+    },
+    getBehaviorCall: function() {
+        // The NAME field is guaranteed to exist, null will never be returned.
+        return /** @type {string} */ (this.getFieldValue('behavior_name'));
+    },
+    /**
+   * Create XML to represent whether the 'divisorInput' should be present.
+   * @return {Element} XML storage element.
+   * @this Blockly.Block
+   */
+    mutationToDom: function() {
+        var container = document.createElement('mutation');
+        var runUntilInput = (this.getFieldValue('execution') == 'until');
+        container.setAttribute('run_until', runUntilInput);
+        if (runUntilInput) {
+            Blockly.CSharp.init(this.workspace);
+            container.setAttribute('run_logic', Blockly.CSharp.valueToCode(this, 'RUN_UNTIL', Blockly.CSharp.ORDER_ATOMIC));
+        } else {
+            container.setAttribute('run_logic', '');
+        }
+        return container;
+    },
+    /**
+     * Parse XML to restore the 'divisorInput'.
+     * @param {!Element} xmlElement XML storage element.
+     * @this Blockly.Block
+     */
+    domToMutation: function(xmlElement) {
+        var runUntilInput = (xmlElement.getAttribute('run_until') == 'true');
+        this.updateShape_(runUntilInput);
+    },
+    /**
+   * Modify this block to have (or not have) an input for 'is divisible by'.
+   * @param {boolean} divisorInput True if this block has a divisor input.
+   * @private
+   * @this Blockly.Block
+   */
+    updateShape_: function(runUntilInput) {
+        // Add or remove a Value Input.
+        var inputExists = this.getInput('RUN_UNTIL');
+        if (runUntilInput) {
+            if (!inputExists) {
+                this.appendValueInput('RUN_UNTIL')
+                    .setCheck('Boolean');
+            }
+        } else if (inputExists) {
+            this.removeInput('RUN_UNTIL');
+        }
+    }
+};
+
+Blockly.Blocks['behavior_simple_count'] = {
     init: function () {
         var thisBlock = this;
         var nameText = "behavior";
