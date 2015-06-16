81b95bb436f914a8d455252da46ca71f7b1296df
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-06-16 10:43:11 +0900

    adding new templates for behavior program description

---
+++ src/Experimot.Scheduler/Scriptcs/ExitBehaviorTemplate.cs
@@ -0,0 +1,8 @@
+public class ExitBehaviorTemplate : IExecuteOnceBehavior
+{
+    public void Execute(IBehaviorExecutionContext context)
+    {
+        // EXIT_BLOCK
+        // EXIT_BLOCK_HERE
+    }
+}
