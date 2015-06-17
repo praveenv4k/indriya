0562ddda02562bf22cd384420eea8e218917459f
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-06-17 11:26:43 +0900

    modifying the program execution

--- src/Experimot.Scheduler/Scriptcs/BehaviorExecutionEngine.cs
+++ src/Experimot.Scheduler/Scriptcs/BehaviorExecutionEngine.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 public class BehaviorExecutionEngine
@@ -15,8 +17,40 @@ public class BehaviorExecutionEngine
         _context = context;
     }
 
+    public static void DisplayLoadedTypes()
+    {
+        var types = Assembly.GetAssembly(typeof (StartupBehavior)).GetTypes().ToList();
+        Console.WriteLine(@"Loaded Types : {0}", string.Join("\n", types.Select(s => s.Name)));
+
+        types = Assembly.GetCallingAssembly().GetTypes().ToList();
+        Console.WriteLine(@"Calling assembly Types : {0}", string.Join("\n", types.Select(s => s.Name)));
+
+        types = Assembly.GetExecutingAssembly().GetTypes().ToList();
+        Console.WriteLine(@"Executing assembly Types : {0}", string.Join("\n", types.Select(s => s.Name)));
+
+        types = Assembly.GetEntryAssembly().GetTypes().ToList();
+        Console.WriteLine(@"Entry assembly Types : {0}", string.Join("\n", types.Select(s => s.Name)));
+    }
+
     public void Run()
     {
+        Console.WriteLine(@"Running ...");
 
+        Console.WriteLine(@"Startup Classes : {0}", string.Join(",", GetTypes("StartupBehavior").Select(s => s.Name)));
+        Console.WriteLine(@"Behavior Classes : {0}",
+            string.Join(",", GetTypes(typeof (ITriggerBehavior)).Select(s => s.Name)));
+        Console.WriteLine(@"Exit Classes : {0}", string.Join(",", GetTypes("ExitBehavior").Select(s => s.Name)));
+    }
+
+    public IList<Type> GetTypes(string className)
+    {
+        return Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.IsClass && t.Name == className).ToList();
+    }
+
+    public IList<Type> GetTypes(Type interfaceType)
+    {
+        return Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.IsClass && interfaceType.IsAssignableFrom(t)).ToList();
     }
 }
