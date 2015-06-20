10c7967d529175bf3380bcc5f3dedb09995cf9df
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-06-20 22:00:10 +0900

    added stat tools, thesis update

--- cloc_execute.bat
+++ cloc_execute.bat
@@ -1,3 +1,4 @@
 REM Computing the source code statistics
 @echo off
-cloc-1.62 --list-file=cloc_include_list.txt --exclude-dir=bin,obj,debug,release,x86,x64
+cloc-1.62 --list-file=cloc_include_list.txt --exclude-dir=bin,obj,debug,release,x86,x64 --out=cloc_code_statistics.csv --csv --ignored=cloc_ignored_list.txt --exclude-ext=*.csproj,*.vcxproj
+cloc-1.62 --list-file=cloc_include_list.txt --exclude-dir=bin,obj,debug,release,x86,x64 --out=docs/Thesis/assets/cloc_code_statistics.csv --csv --exclude-ext=*.csproj,*.vcxproj
