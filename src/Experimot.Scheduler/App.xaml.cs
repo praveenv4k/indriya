f2faabbee102ada5416a40f4383fd9b4eecf8e0d
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2015-07-01 17:40:39 +0900

    Fix the bugs in approach behavior

--- src/Experimot.Scheduler/App.xaml.cs
+++ src/Experimot.Scheduler/App.xaml.cs
@@ -34,6 +34,7 @@ namespace Experimot.Scheduler
 
         private void RunTests(experimot_config config)
         {
+            Tests.Test.CheckRelativeAngle();
             //Tests.Test.ExecuteParallel();
             //Tests.Test.TestVoiceCommandJson();
             //Tests.Test.TestSharpDxMatrixRotation();
